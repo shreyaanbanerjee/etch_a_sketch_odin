3783181ab2a07e95e75d3c02172e295758fccdb1
Shreyaan <shre6745@gmail.com>
2024-05-13 18:03:59 +0530

    changes

---
+++ script.js
@@ -0,0 +1,56 @@
+const container = document.querySelector(".container");
+const button =document.querySelector(".butt");
+let dimensions;
+let d;  
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+function createGridCell() {
+  const cell = document.createElement("div");
+  cell.classList.add("grid-cell");
+  return cell;
+}
+function make_grid(val=16)
+{
+for (let i = 0; i < val; i++) {
+  const row = document.createElement("div");
+  row.classList.add("grid-row");
+  row.style.display = "flex";
+
+  for (let j = 0; j < val; j++) {
+    const cell = createGridCell();
+    row.appendChild(cell);
+  }
+
+  container.appendChild(row);
+}}
+make_grid(dimensions);
+button.addEventListener("click",()=>
+{
+    let p=prompt("Enter new dimensions","16");
+    if(p!=null)
+        {
+            dimensions=p;
+            while (container.firstChild) {
+                container.firstChild.remove()
+            }
+           make_grid(dimensions);
+        //    let d=960/p;
+        //    document.querySelector(".grid-cell").setAttribute("width",d);
+        //    document.querySelector(".grid-cell").setAttribute("height",d);
+        }
+})
+const cells=document.querySelectorAll(".grid-cell");
+cells.forEach(cell => {
+    cell.addEventListener("mouseover",()=>
+    {
+        let rand_color=getRandomColor();
+      cell.style.backgroundColor=rand_color;
+    //   cell.style.opacity=cell.style.opacity*1.1;
+    })
+});
